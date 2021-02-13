1b35d0bcb5efbb02f9bf6d86e3f797c1c710d471
Odeta Maslovaite <>
2021-02-13 13:40:09 +0100

    creating routes for restaurant model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'pages#home'
 
-  get '/about', to: "pages#about"
-  get '/contact', to: "pages#contact"
-  #verb         to: controller/action
+  # CRUD
+  # READ all Restaurant
+  get '/restaurants', to: 'restaurants#index'
+  # READ one restaurant
+  get '/restaurant/:id', to: 'restaurants#show'
+  # CREATE Restaurant
+  get '/restaurants/new', to: 'restaurants#new'
+  post '/restaurants', to: 'restaurants#create'
+  # UPDATE REstauranta
+  get '/restaurants/:id/edit', to: 'restaurants#edit'
+  patch '/restaurants/:id', to: 'restaurants#update'
+  # DELETE restaurant
+  delete '/restaurants/:id' to: 'restaurants#destroy'
+
 end
