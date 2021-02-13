d800aa6cb2da896429bfc2e65ce2108868872efe
Odeta Maslovaite <>
2021-02-13 13:45:29 +0100

    updateing routes with one line of code

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,18 @@ Rails.application.routes.draw do
 
   # CRUD
   # READ all Restaurant
-  get '/restaurants', to: 'restaurants#index'
-  # READ one restaurant
-  get '/restaurant/:id', to: 'restaurants#show'
-  # CREATE Restaurant
-  get '/restaurants/new', to: 'restaurants#new'
-  post '/restaurants', to: 'restaurants#create'
-  # UPDATE REstauranta
-  get '/restaurants/:id/edit', to: 'restaurants#edit'
-  patch '/restaurants/:id', to: 'restaurants#update'
-  # DELETE restaurant
-  delete '/restaurants/:id' to: 'restaurants#destroy'
+  # get '/restaurants', to: 'restaurants#index'
+  # # READ one restaurant
+  # get '/restaurant/:id', to: 'restaurants#show'
+  # # CREATE Restaurant
+  # get '/restaurants/new', to: 'restaurants#new'
+  # post '/restaurants', to: 'restaurants#create'
+  # # UPDATE REstauranta
+  # get '/restaurants/:id/edit', to: 'restaurants#edit'
+  # patch '/restaurants/:id', to: 'restaurants#update'
+  # # DELETE restaurant
+  # delete '/restaurants/:id', to: 'restaurants#destroy'
 
+  # this one line is equal to all the routes&actions above
+  resources :restaurants
 end
